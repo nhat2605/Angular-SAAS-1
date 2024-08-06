37f75499c35d03e907035305abf48dc7ae509ce0
Nhat <lale6280@uni.sydney.edu.au>
2024-08-06 22:06:27 +1000

    improve mobile

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
 import { AuthenticationService } from '../../authentication/authentication.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
-  styles: ``
+
 })
 export class HeaderComponent {
+  isMenuOpen = false;
   constructor(private router: Router, public authService: AuthenticationService) {}
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
   logout() {
     this.authService.clearLoginToken();
